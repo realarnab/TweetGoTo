017d4b50b3505ffe2bc18950bee11e171f590c2d
Arnab Mondal <arnabmondal604@gmail.com>
2024-10-08 17:59:03 +0530

    validation added

--- db/migrate/20241003131943_create_users.rb
+++ db/migrate/20241003131943_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.2]
   def change
     create_table :users do |t|
-      t.string :email
+      t.string :email, null: false # email should not be null
       t.string :user_password
 
       t.timestamps
