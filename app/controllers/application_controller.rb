206e65c4ff26dca90f3d455449284941be2b6de1
Arnab Mondal <arnabmondal604@gmail.com>
2024-10-13 18:47:17 +0530

    user session implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
+
+  # before any action is performed it will call set_current_user method
+  before_action :set_current_user
+
+  # so that current user can be tracked by all the controller
+  def set_current_user
+    if session[:user_id]
+      Current.user = User.find_by(id: session[:user_id])
+    end
+  end
 end
