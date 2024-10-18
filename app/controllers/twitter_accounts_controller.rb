e7c4facedcb740aa11bf89319051dd9e7b36a429
Arnab Mondal <arnabmondal604@gmail.com>
2024-10-18 19:50:38 +0530

    new method added

--- app/controllers/twitter_accounts_controller.rb
+++ app/controllers/twitter_accounts_controller.rb
@@ -1,6 +1,6 @@
 class TwitterAccountsController < ApplicationController
   before_action :require_user_logged_in!
-  before_action :set_twitter_account, only: [ :show, :destroy ]
+  before_action :set_twitter_account, only: [ :destroy ]
   def index
     @twitter_accounts = Current.user.twitter_accounts
   end
