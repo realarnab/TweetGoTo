ae436a635fe384f4de29fdd357689c18117133b3
Arnab Mondal <arnabmondal604@gmail.com>
2024-10-19 13:30:16 +0530

    validation added

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,15 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   belongs_to :twitter_account
+
+  validates :body, length: { minimun: 1, maximum: 280 }
+  validates :publish_at, presence: true
+
+  after_initialize do
+    self.publish_at ||= 24.hours.from_now
+  end
+
+  def published?
+    tweet_id?
+  end
 end
