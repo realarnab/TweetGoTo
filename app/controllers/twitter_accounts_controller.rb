67956c8b81a814d3f0ee406a42b015f9d9bac561
Arnab Mondal <arnabmondal604@gmail.com>
2024-10-22 10:44:22 +0530

    comments added

--- app/controllers/twitter_accounts_controller.rb
+++ app/controllers/twitter_accounts_controller.rb
@@ -1,7 +1,10 @@
 class TwitterAccountsController < ApplicationController
   before_action :require_user_logged_in!
+  # before deleting the twitter account, user needs to connected with a twitter account
   before_action :set_twitter_account, only: [ :destroy ]
+
   def index
+    # finds all the twitter account associated with the user from the twitter_accounts method
     @twitter_accounts = Current.user.twitter_accounts
   end
 
