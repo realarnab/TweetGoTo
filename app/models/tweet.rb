6bb7733c611a296b3361a3b3c24c80920ab11f10
Arnab Mondal <arnabmondal604@gmail.com>
2024-10-22 10:45:24 +0530

    comments added

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,16 +5,26 @@ class Tweet < ApplicationRecord
   validates :body, length: { minimun: 1, maximum: 280 }
   validates :publish_at, presence: true
 
+  # this callbacks set a default value for publish_at if not set by user
   after_initialize do
     self.publish_at ||= 24.hours.from_now
   end
 
+  # it will triggered after tweet is saved to the db / publish_at attribute change- it schedule a b/g job in TweetJob
+  after_save_commit do
+    if publish_at_previously_changed? # it will check the pulish_at is changed or not
+      TweetJob.set(wait_until: publish_at).perform_later(self)
+    end
+  end
+
+  # it returns true if the tweet_id is present means tweet is already published
   def published?
     tweet_id?
   end
 
+  # it is responsible for publishing the tweet over X api
   def publish_to_twitter!
-    tweet = twitter_account.client.update(body)
-    update(tweet_id: tweet.id)
+    tweet = twitter_account.client.update(body) # this send the tweet via X api
+    update(tweet_id: tweet.id) # capture the tweet id
   end
 end
--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -1,14 +1,29 @@
+# class TwitterAccount < ApplicationRecord
+#   belongs_to :user
+#   has_many :tweets
+#   validates :username, uniqueness: true
+
+#   def client
+#     Twitter::REST::Client.new do |config|
+#       config.consumer_key         = Rails.application.credentials.dig(:twitter, :api_key)
+#       config.consumer_secret      = Rails.application.credentials.dig(:twitter, :api_secret)
+#       config.access_token         = token
+#       config.access_token_secret  = secret
+#     end
+#   end
+# end
+
 class TwitterAccount < ApplicationRecord
   belongs_to :user
   has_many :tweets
   validates :username, uniqueness: true
 
   def client
-    Twitter::REST::Client.new do |config|
-      config.consumer_key         = Rails.application.credentials.dig(:twitter, :api_key)
-      config.consumer_secret      = Rails.application.credentials.dig(:twitter, :api_secret)
-      config.access_token         = token
-      config..access_token_secret = secret
-    end
+    X::Client.new(
+      api_key:             Rails.application.credentials.dig(:twitter, :api_key),
+      api_key_secret:      Rails.application.credentials.dig(:twitter, :api_secret),
+      access_token:        token,
+      access_token_secret: secret
+    )
   end
 end
